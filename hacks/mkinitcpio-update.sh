9bb87eeec2040c0390f1214672a9b6c683face03
Matthew Anderson <ruinairas1992@gmail.com>
2023-01-17 23:30:57 -0600

    Add control for mkinitfs -P commands

---
+++ hacks/mkinitcpio-update.sh
@@ -0,0 +1,16 @@
+#!/bin/bash/
+
+if [ $EUID -ne 0 ]; then
+	echo "$(basename $0) must be run as root"
+	exit 1
+fi
+## Handling mkinitpio -P sucks on ChimeraOS..lets fix that
+cp /boot/chimeraos-*/* /boot
+
+mkinitcpio -P
+
+cp -a /boot/initramfs-linux.img /boot/chimeraos-*/
+
+rm /boot/initramfs*
+
+rm /boot/vmlinuz-linux
