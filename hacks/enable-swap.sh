064a9e965e9095059f9da3de3fa82f9d8cd6848d
Matthew Anderson <ruinairas1992@gmail.com>
2023-02-15 21:45:33 -0600

    Add swap creation tool

---
+++ hacks/enable-swap.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+if [ $EUID -ne 0 ]; then
+	echo "$(basename $0) must be run as root"
+	exit 1
+fi
+
+cd ~/
+
+truncate -s 0 swapfile
+chattr +C swapfile
+fallocate -l 4G swapfile
+chmod 0600 swapfile
+mkswap swapfile
+swapon swapfile
+
+
+echo "/home/gamer/swapfile        none        swap        defaults      0 0" >> /etc/fstab
+
+echo "Reboot for changes to take effect"
+
