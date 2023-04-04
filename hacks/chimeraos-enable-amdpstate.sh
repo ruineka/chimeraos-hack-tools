95aac31eebd5faf2be0f0cbc80f6a1241a522128
Matthew Anderson <ruinairas1992@gmail.com>
2023-04-04 13:46:40 -0500

    Update amd_pstates script for Systemd-boot

--- hacks/chimeraos-enable-amdpstate.sh
+++ hacks/chimeraos-enable-amdpstate.sh
@@ -6,12 +6,12 @@ if [ $EUID -ne 0 ]; then
 fi
 
 # Change $kernel_parameters to whatever you want to be set for your kernel
-kernel_parameters="quiet splash loglevel=3 rd.systemd.show_status=auto rd.udev.log_priority=3 ibt=off split_lock_detect=off amd_pstate=passive"
+kernel_parameters="quiet splash loglevel=3 rd.systemd.show_status=auto rd.udev.log_priority=3 ibt=off split_lock_detect=off amd_pstate=passive iomem=relaxed"
 
 mkdir -p /run/media/boot
 mount -L frzr_efi /run/media/boot
 
-sed -i "s/quiet splash/${kernel_parameters}/g" /run/media/boot/EFI/BOOT/syslinux.cfg
+sed -i "s/quiet splash/${kernel_parameters}/g" /run/media/boot/loader/entries/frzr.conf
 umount -l /run/media/boot
 rm -r /run/media/boot
 
